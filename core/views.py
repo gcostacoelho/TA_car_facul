c54068910a564468aa42854f7f2ddacbbd1a407e
gcostacoelho <gcostacoelho2003@gmail.com>
2022-06-09 21:53:29 -0300

    last-adjusts

--- core/views.py
+++ core/views.py
@@ -16,167 +16,238 @@ class Registrar(generic.CreateView):
 
 #Cliente
 def cadastro_cliente(request):
-    form = FormCliente(request.POST or None, request.FILES or None)
-    if form.is_valid():
-        nome = form.cleaned_data['nome']
-        form.save()
-        messages.success(request, f'Cliente {nome} cadastrado com sucesso!')
-        return redirect('url_lista_clientes')
-    
-    contexto = {'form': form, 'titulo': 'Cadastro de cliente', 'stringBotao': 'Cadastrar'}
-    return render(request, 'core/cadastro.html', contexto)
-"""    render = É uma resposta sem código para quem solicitou acesso ao server"""
+    try:
+        form = FormCliente(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            form.save()
+            messages.success(request, f'Cliente {nome} cadastrado com sucesso!')
+            return redirect('url_lista_clientes')
+        
+        contexto = {'form': form, 'titulo': 'Cadastro de cliente', 'stringBotao': 'Cadastrar'}
+        return render(request, 'core/cadastro.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
+    """    render = É uma resposta sem código para quem solicitou acesso ao server"""
 
 def lista_cliente(request):
-    dados = Cliente.objects.all()
-    if request.POST:
-        if request.POST['pesquisa']: dados = Cliente.objects.filter(nome=request.POST['pesquisa'])
+    try:
+        dados = Cliente.objects.all()
+        if request.POST:
+            if request.POST['pesquisa']: dados = Cliente.objects.filter(nome=request.POST['pesquisa'])
 
-    contexto = {'dados': dados}
-    return render(request, 'core/lista_cliente.html', contexto)
+        contexto = {'dados': dados}
+        return render(request, 'core/lista_cliente.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 def altera_cliente(request, id):
-    objeto = Cliente.objects.get(id=id)
-    form = FormCliente(request.POST or None, request.FILES or None, instance=objeto)
-    
-    if form.is_valid():
-        nome = form.cleaned_data['nome']
-        form.save()
-        messages.success(request, f'Cliente {nome} atualizado com sucesso')
-        return redirect('url_lista_clientes')
-    
-    contexto = {'form': form, 'titulo': 'Atualização de clientes', 'stringBotao': 'Atualizar'}
-    return render(request, 'core/cadastro.html', contexto)
+    try:
+        objeto = Cliente.objects.get(id=id)
+        form = FormCliente(request.POST or None, request.FILES or None, instance=objeto)
+        
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            form.save()
+            messages.success(request, f'Cliente {nome} atualizado com sucesso')
+            return redirect('url_lista_clientes')
+        
+        contexto = {'form': form, 'titulo': 'Atualização de clientes', 'stringBotao': 'Atualizar'}
+        return render(request, 'core/cadastro.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 def exclui_cliente(request, id):
-    objeto = Cliente.objects.get(id=id)
-    if request.POST:
-        objeto.delete()
-        return redirect('url_lista_clientes')
-    contexto = {'url': '/lista_clientes', 'objeto': objeto.nome}
-    return render(request, 'core/mensagem_excluir.html', contexto)
+    try:
+        objeto = Cliente.objects.get(id=id)
+        if request.POST:
+            objeto.delete()
+            return redirect('url_lista_clientes')
+        contexto = {'url': '/lista_clientes', 'objeto': objeto.nome}
+        return render(request, 'core/mensagem_excluir.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 #Veiculos
 def cadastro_veiculo(request):
-    form = FormVeiculo(request.POST or None, request.FILES or None)
-    if form.is_valid():
-        form.save()
-        return redirect('url_principal')
-    contexto = {'form': form, 'titulo': 'Cadastro de veículos', 'stringBotao': 'Cadastrar'}
-    return render(request, 'core/cadastro.html', contexto)
+    try:
+        form = FormVeiculo(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            form.save()
+            return redirect('url_principal')
+        contexto = {'form': form, 'titulo': 'Cadastro de veículos', 'stringBotao': 'Cadastrar'}
+        return render(request, 'core/cadastro.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 def lista_veiculo(request):
-    dados = Veiculo.objects.all()
-    if request.POST:
-        if request.POST['pesquisa']: dados=Veiculo.objects.filter(placa=request.POST['pesquisa'])
-    contexto = {'dados': dados}
-    return render(request, 'core/lista_veiculo.html', contexto)
+    try:
+        dados = Veiculo.objects.all()
+        if request.POST:
+            if request.POST['pesquisa']: dados=Veiculo.objects.filter(placa=request.POST['pesquisa'])
+        contexto = {'dados': dados}
+        return render(request, 'core/lista_veiculo.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 def altera_veiculo(request, id):
-    objeto  = Veiculo.objects.get(id=id)
-    form = FormVeiculo(request.POST or None, request.FILES or None, instance=objeto)
+    try:
+        objeto  = Veiculo.objects.get(id=id)
+        form = FormVeiculo(request.POST or None, request.FILES or None, instance=objeto)
 
-    if form.is_valid():
-        nome = form.cleaned_data['modelo']
-        form.save()
-        messages.success(request, f'{nome} atualzado com sucesso')
-        return redirect('url_lista_veiculos')
-    
-    contexto = {'form': form, 'titulo': 'Atualização de veículos', 'stringBotao': 'Atualizar'}
-    return render(request, 'core/cadastro.html', contexto)
+        if form.is_valid():
+            nome = form.cleaned_data['modelo']
+            form.save()
+            messages.success(request, f'{nome} atualzado com sucesso')
+            return redirect('url_lista_veiculos')
+        
+        contexto = {'form': form, 'titulo': 'Atualização de veículos', 'stringBotao': 'Atualizar'}
+        return render(request, 'core/cadastro.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 def exclui_veiculo(request, id):
-    objeto = Veiculo.objects.get(id=id)
-    if request.POST:
-        objeto.delete()
-        return redirect('url_lista_veiculos')
-    contexto = {'url': '/lista_veiculos', 'objeto': objeto.modelo}
-    return render(request, 'core/mensagem_excluir.html', contexto)
+    try:
+        objeto = Veiculo.objects.get(id=id)
+        if request.POST:
+            objeto.delete()
+            return redirect('url_lista_veiculos')
+        contexto = {'url': '/lista_veiculos', 'objeto': objeto.modelo}
+        return render(request, 'core/mensagem_excluir.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 #Fabricante
 def cadastro_fabricante(request):
-    form = FormFabricante(request.POST or None, request.FILES or None)
-    if form.is_valid():
-        form.save()
-        return redirect('url_principal')
-    contexto = {'form': form, 'titulo': 'Cadastro de fabricantes', 'stringBotao': 'Cadastrar'}
-    return render(request, 'core/cadastro.html', contexto)
+    try:
+        form = FormFabricante(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            form.save()
+            return redirect('url_principal')
+        contexto = {'form': form, 'titulo': 'Cadastro de fabricantes', 'stringBotao': 'Cadastrar'}
+        return render(request, 'core/cadastro.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 def lista_fabricante(request):
-    dados = Fabricante.objects.all()
-    contexto = {'dados': dados}
-    return render(request, 'core/lista_fabricante.html', contexto)
+    try:
+        dados = Fabricante.objects.all()
+        contexto = {'dados': dados}
+        return render(request, 'core/lista_fabricante.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 #Preços
 def tabela_preco(request):
-    dados = Preco.objects.all()
-    contexto = {'dados': dados}
-    return render(request, 'core/tabela_preco.html', contexto)
+    try:
+        dados = Preco.objects.all()
+        contexto = {'dados': dados}
+        return render(request, 'core/tabela_preco.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 #Rotativo
 def cadastro_rotativo(request):
-    form = FormCadastroRotativo(request.POST or None, request.FILES or None)
-    if form.is_valid():
-        form.save()
-        return redirect('url_lista_rotativo')
-    contexto = {'form': form, 'titulo':'Cadastro de rotativo', 'stringBotao': 'Cadastrar', 'calendario': True}
-    return render(request, 'core/cadastro.html', contexto)
+    try:
+        form = FormCadastroRotativo(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            form.save()
+            return redirect('url_lista_rotativo')
+        contexto = {'form': form, 'titulo':'Cadastro de rotativo', 'stringBotao': 'Cadastrar', 'calendario': True}
+        return render(request, 'core/cadastro.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 def listagem_rotativo(request):
-    dados = Rotativo.objects.all()
-    if request.POST:
-        if request.POST['pesquisa']: dados=Rotativo.objects.filter(id_veiculo=int(request.POST['pesquisa']))
-    contexto = {'dados': dados}
-    return render(request, 'core/lista_rotativo.html', contexto)
+    try:
+        dados = Rotativo.objects.all()
+        if request.POST:
+            if request.POST['pesquisa']: dados=Rotativo.objects.filter(id_veiculo=int(request.POST['pesquisa']))
+        contexto = {'dados': dados}
+        return render(request, 'core/lista_rotativo.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 def altera_rotativo(request, id):
+    try:
+        objeto = Rotativo.objects.get(id=id)
+        form = FormRotativo(request.POST or None, request.FILES or None, instance=objeto)
 
-    objeto = Rotativo.objects.get(id=id)
-    form = FormRotativo(request.POST or None, request.FILES or None, instance=objeto)
-
-    if form.is_valid():
-        objeto.calcula_total()
-        veiculo = form.cleaned_data['id_veiculo']
-        form.save()
-        messages.success(request, f'{veiculo} atualizado com sucesso')
-        return redirect('url_lista_rotativo')
-        
-    contexto = {'form': form, 'titulo':'Atualização de rotativo', 'stringBotao': 'Atualizar', 'calendario': True}
-    return render(request, 'core/cadastro.html', contexto)
+        if form.is_valid():
+            objeto.calcula_total()
+            veiculo = form.cleaned_data['id_veiculo']
+            form.save()
+            messages.success(request, f'{veiculo} atualizado com sucesso')
+            return redirect('url_lista_rotativo')
+            
+        contexto = {'form': form, 'titulo':'Atualização de rotativo', 'stringBotao': 'Atualizar', 'calendario': True}
+        return render(request, 'core/cadastro.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 #Mensalista
 def cadastro_mensalista(request):
-    form = FormMensalista(request.POST or None, request.FILES or None)
-    if form.is_valid():
-        form.save()
-        return redirect('url_lista_mensalista')
-    contexto = {'form': form, 'titulo':'Cadastro de mensalista', 'stringBotao': 'Cadastrar'}
-    return render(request, 'core/cadastro.html', contexto)
+    try:
+        form = FormMensalista(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            form.save()
+            return redirect('url_lista_mensalista')
+        contexto = {'form': form, 'titulo':'Cadastro de mensalista', 'stringBotao': 'Cadastrar'}
+        return render(request, 'core/cadastro.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 def listagem_mensalista(request):
-    dados = Mensalista.objects.all()
-    if request.POST:
-        if request.POST['pesquisa']: dados=Mensalista.objects.filter(id_cliente=int(request.POST['pesquisa']))
-    contexto = {'dados': dados}
-    return render(request, 'core/lista_mensalista.html', contexto)
+    try:
+        dados = Mensalista.objects.all()
+        if request.POST:
+            if request.POST['pesquisa']: dados=Mensalista.objects.filter(id_cliente=int(request.POST['pesquisa']))
+        contexto = {'dados': dados}
+        return render(request, 'core/lista_mensalista.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 def altera_mensalista(request, id):
-    objeto = Mensalista.objects.get(id=id)
-    form = FormMensalista(request.POST or None, request.FILES or None, instance=objeto)
+    try:
+        objeto = Mensalista.objects.get(id=id)
+        form = FormMensalista(request.POST or None, request.FILES or None, instance=objeto)
 
-    if form.is_valid():
-        nome = form.cleaned_data['id_cliente']
-        form.save()
-        messages.success(request, f'Mensalista {nome} atualizado com sucesso')
-        return redirect('url_lista_mensalista')
+        if form.is_valid():
+            nome = form.cleaned_data['id_cliente']
+            form.save()
+            messages.success(request, f'Mensalista {nome} atualizado com sucesso')
+            return redirect('url_lista_mensalista')
 
-    contexto = {'form': form, 'titulo': 'Atualização de mensalistas', 'stringBotao': 'Atualizar'}
-    return render(request, 'core/cadastro.html', contexto)
+        contexto = {'form': form, 'titulo': 'Atualização de mensalistas', 'stringBotao': 'Atualizar'}
+        return render(request, 'core/cadastro.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
 
 def exclui_mensalista(request, id):
-    objeto = Mensalista.objects.get(id=id)
-    if request.POST:
-        objeto.delete()
-        return redirect('url_lista_mensalista')
-    contexto = {'url': '/lista_mensalista', 'objeto': objeto.id_cliente}
-    return render(request, 'core/mensagem_excluir.html', contexto)
+    try:
+        objeto = Mensalista.objects.get(id=id)
+        if request.POST:
+            objeto.delete()
+            return redirect('url_lista_mensalista')
+        contexto = {'url': '/lista_mensalista', 'objeto': objeto.id_cliente}
+        return render(request, 'core/mensagem_excluir.html', contexto)
+    except Exception as mensagem_erro:
+        contexto = {'msg_erro': mensagem_erro}
+        return render(request, '500.html', contexto)
--- project4/settings.py
+++ project4/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*nn0i=&85r3=3@)p-q_9vq=n@$3lm93%^tn%=otwjg4)@!hyfh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
