56f28fd66245fb07e8a370dfcc8fe474ed48f05d
gcostacoelho <gcostacoelho2003@gmail.com>
2022-06-25 20:45:04 -0300

    adjusts-deploy

--- project4/settings.py
+++ project4/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR + '/static', BASE_DIR + '/static/img']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files')
