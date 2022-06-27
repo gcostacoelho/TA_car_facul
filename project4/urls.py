1297f1c0176686964e6e998d6d9475465d137bd8
gcostacoelho <gcostacoelho2003@gmail.com>
2022-06-27 17:56:56 -0300

    adjusts-deploy

--- project4/urls.py
+++ project4/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/registrar', Registrar.as_view(), name="url_registrar"),
-    path('/index.html', home, name='url_principal'),
+    path('index/', home, name='url_principal'),
 
     #Clientes
     path('cadastro_cliente/', cadastro_cliente),
