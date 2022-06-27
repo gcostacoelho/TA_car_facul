0d5d639c1b8cb5016fa56e7d2b156a956fb04484
gcostacoelho <gcostacoelho2003@gmail.com>
2022-06-27 19:50:27 -0300

    adjusts-deploy

--- project4/wsgi.py
+++ project4/wsgi.py
@@ -15,6 +15,5 @@ from dj_static  import  Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project4.settings')
 
-application = get_wsgi_application()
 application = Cling( get_wsgi_application() ) 
 
